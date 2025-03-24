ffbe136c31e31ac07e409396d865c356520ed1db
Thiago <thiagodelgado.dev@gmail.com>
2025-03-24 14:09:34 -0300

    implement user domain model with validation and repository interface

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,11 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    "eslint-disable-next-line prettier/prettier": "off",
   },
+  "prettier/prettier":[
+    "error",
+    {
+      "endOfLine": "auto",
+    }
+  ]
 };
