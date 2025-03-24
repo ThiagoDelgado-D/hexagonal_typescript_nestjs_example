3f6ad9019a98628929771b8d26d3c828679f5ca9
Thiago <thiagodelgado.dev@gmail.com>
2025-03-24 14:57:47 -0300

    add user module and controller; configure TypeORM with PostgreSQL and environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './lib/User/infraestructure/NestJs/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}.local`,
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URL'),
+        entities: [],
+        synchronize: true,
+      }),
+    }),
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
